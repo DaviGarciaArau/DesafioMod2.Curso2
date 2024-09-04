f05c4dfea7f076c44dbc7ee43bc80d29893318ac
Davi <Davi@DESKTOP-3F8IGG3>
2024-09-04 20:33:09 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioMod2.Curso2/CodeFile1.cs
@@ -0,0 +1,35 @@
+﻿//class Conta
+//{
+// public string Titular { get; set; }
+// public int Saldo { get; set; }
+
+// public void ExibirFichaTecnica()
+//  {
+//     Console.WriteLine($"Titular: {Titular}");
+//     Console.WriteLine($"Saldo: {Saldo}");
+
+// }
+//}
+
+
+//Desenvolver a classe Produto, com os atributos nome, marca, preco e estoque. Além disso, garantir que o preço
+//e o estoque do produto sejam valores positivos e criar uma propriedade que mostra detalhadamente as informações do produto,
+//para que seja usado pela equipe de vendas.
+
+class Produto
+{
+    public string Nome { get; set; }
+    public string Marca { get; set; }
+    public int Preco { get; set; }
+    public bool Estoque { get; set; }
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Marca: {Marca}");
+        Console.WriteLine($"Preço: {Preco}");
+        Console.WriteLine($"Estoque: {Estoque}");
+    }
+}
+
---
+++ DesafioMod2.Curso2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Conta conta = new Conta();
+
+//conta.Titular = ("Roberto");
+//conta.Saldo = 1324;
+
+
+//conta.ExibirFichaTecnica();
+
+//Desenvolver a classe Produto, com os atributos nome, marca, preco e estoque. Além disso, garantir que o preço
+//e o estoque do produto sejam valores positivos e criar uma propriedade que mostra detalhadamente as informações do produto,
+//para que seja usado pela equipe de vendas.
+
+
+Produto produto = new Produto();
+
+produto.Nome = ("Papel higiênico");
+produto.Marca = ("Neve");
+produto.Estoque = (true);
+produto.Preco = 19;
+
+produto.ExibirFichaTecnica();
